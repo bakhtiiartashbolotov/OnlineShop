f4015c1ebb8e49f320b493121a3c944af9bbfbeb
bakhtiiartashbolotov <bakhtiiar.tashbolotov@alatoo.edu.kg>
2023-12-03 11:10:32 +0600

    added styles for auth pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, render
+from onlineshopapp.models import Category
+
+
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    template_name = 'registration/signup.html'  
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
